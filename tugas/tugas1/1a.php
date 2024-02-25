d7775bf2a07d0166dd9114deb017d70ae372bfd6
unknown <deawardini90@gmail.com>
2024-02-25 07:45:14 +0700

    first commit

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,8 @@
+<?php
+
+$topi = "Topi";
+$bundar  = "Bundar";
+
+echo '<h2 style="font-style: italic;">"' . $topi . ' saya ' . $bundar . ', ' . $bundar . ' ' .  $topi . ' saya."</h2>';
+
+?>
---
+++ tugas/tugas2/2a.php
@@ -0,0 +1,18 @@
+<?php 
+
+$first_name = "Dea";
+$last_name ="Wardini";
+
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 3 == 0 and $i % 5 == 0) {
+        echo $first_name . $last_name . "<br>";
+    } else if ($i % 3 == 0) {
+        echo $first_name . "<br>";
+    } else if ($i % 5 == 0) {
+        echo $last_name . "<br>";
+    } else {
+        echo $i . "<br>";
+    }
+}
+
+?>
