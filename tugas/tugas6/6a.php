bb2142354ca0e7c02a9f82017bd096dc6186112c
unknown <deawardini90@gmail.com>
2024-03-24 22:15:05 +0700

    tugas 6

---
+++ tugas/tugas6/6a.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 6a</title>
+
+    <style>
+        .box2,
+        .box{
+            display: inline-block;
+            width: 20px;
+            height: 20px;
+            background-color: #ffc0cb;
+            color: black;
+            text-align: center;
+            line-height: 20px;
+            border: 1px solid black;
+            margin : 3px;
+        }
+
+        .box2{
+            background-color: white;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    if(isset($_GET['angka'])){
+        $angka = $_GET['angka'];
+    for($i =$angka; $i>=1; $i--){
+    for($j=1; $j<=$i; $j++){
+        if($i%2==0){
+            echo '<div class="box">'.$i.'</div>';
+        }else{
+            echo '<div class="box2">'.$i.'</div>';
+        }
+    }
+    echo "<br>";
+}
+}else{}
+?>
+    
+</body>
+</html>
